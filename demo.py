7655afbb0e854d731db5096833d65d74d75b8e70
dipti552 <144344430+dipti552@users.noreply.github.com>
2025-05-22 09:02:51 +0545

    Add files via upload
    Started practicing some basics

---
+++ demo.py
@@ -0,0 +1,18 @@
+#madlibs game
+#fill the words to create a story
+adjective=input("enter a adjective(description)")
+noun=input("enter a noun")
+verb1=input("enter a verb ending with '-ing'")
+verb2=input("enter a verb (emotions)")
+print(f"today is a {adjective} day")
+print(f"I wanted to go to {noun}")
+print(f"But i got struck by a {verb1} monkey")
+print(f"It was very {verb2}")
+
+#shopping cart program
+item=input("what item is selected")
+price=float(input("what is the price?"))
+quantity=int(input("what is the quantity?"))
+print(f"you selected : {quantity} * {item}")
+print(price*quantity)
+      
